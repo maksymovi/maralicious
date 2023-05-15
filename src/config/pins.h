9c276923c12583b204db568aa4d3a6d011df65d0
Maksym Prokopovych <maksymovi@maksym.io>
2023-05-14 20:02:24 -0700

    Small changes to port to T Display S3

--- src/config/pins.h
+++ src/config/pins.h
@@ -1,5 +1,12 @@
 #pragma once
 
+//DEFINE ONE AND ONLY ONE
+
+//#define T_DISPLAY
+#define T_DISPLAY_S3
+
+#ifdef T_DISPLAY
+
 #define PUMP_PIN 27
 
 //black yellow
@@ -9,3 +16,18 @@
 
 #define BUTTONA_PIN  0
 #define BUTTONB_PIN  35
+
+
+#elif defined(T_DISPLAY_S3)
+
+#define PUMP_PIN 16
+
+//black yellow
+#define MARAX_TX_PIN 12
+//white orange
+#define MARAX_RX_PIN 13
+
+#define BUTTONA_PIN  0
+#define BUTTONB_PIN  35
+
+#endif
